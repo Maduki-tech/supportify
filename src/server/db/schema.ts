9118abebd8a37838913bf29ea5da043740123039
Maduki-tech <d.schlueter1011@gmail.com>
2026-03-10 21:34:07 +0100

    Added org and project table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,8 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { index, pgTableCreator } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { pgTableCreator } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -11,16 +12,49 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  */
 export const createTable = pgTableCreator((name) => `supportify_${name}`);
 
-export const posts = createTable(
-    "post",
+export const users = createTable(
+    "user",
     (d) => ({
         id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-        name: d.varchar({ length: 256 }),
-        createdAt: d
-            .timestamp({ withTimezone: true })
-            .$defaultFn(() => /* @__PURE__ */ new Date())
+        clearkID: d.varchar({ length: 256 }).notNull(),
+    })
+)
+
+export const organization = createTable(
+    "organization",
+    (d) => ({
+        id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+        name: d.varchar({ length: 256 }).notNull(),
+        createdAt: d.timestamp({ withTimezone: true })
+            .defaultNow()
             .notNull(),
         updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+        deletedAt: d.timestamp({ withTimezone: true }),
+
+    })
+)
+
+export const projects = createTable(
+    "project",
+    (d) => ({
+        id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+        name: d.varchar({ length: 256 }).notNull(),
+        organizationID: d.integer().references(() => organization.id).notNull(),
+        createdAt: d.timestamp({ withTimezone: true })
+            .defaultNow()
+            .notNull(),
+        updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+        deletedAt: d.timestamp({ withTimezone: true }),
+    })
+)
+
+export const organizationRelation = relations(organization, ({ many }) => ({
+    projects: many(projects),
+}))
+
+export const projectRelation = relations(projects, ({ one }) => ({
+    organization: one(organization, {
+        fields: [projects.organizationID],
+        references: [organization.id],
     }),
-    (t) => [index("name_idx").on(t.name)],
-);
+}))
