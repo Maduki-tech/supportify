c77d46c70dee9a10d47d73d6779e12cd6c852cbc
Maduki-tech <d.schlueter1011@gmail.com>
2026-03-14 22:11:58 +0100

    feat: updated db schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,22 +1,20 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
 import { relations } from "drizzle-orm";
 import { pgTableCreator } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `supportify_${name}`);
+// TODO: Maybe change id with uuid some day
 
 export const users = createTable(
     "user",
     (d) => ({
         id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-        clearkID: d.varchar({ length: 256 }).notNull(),
+        clerkID: d.varchar({ length: 256 }).notNull(),
+        orgID: d.integer().references(() => organization.id).notNull(),
+        createdAt: d.timestamp({ withTimezone: true })
+            .defaultNow()
+            .notNull(),
+        updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+        deletedAt: d.timestamp({ withTimezone: true }),
     })
 )
 
@@ -48,8 +46,25 @@ export const projects = createTable(
     })
 )
 
+export const tickets = createTable(
+    "ticket",
+    (d) => ({
+        id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+        title: d.varchar({ length: 256 }).notNull(),
+        description: d.text().notNull(),
+        projectID: d.integer().references(() => projects.id).notNull(),
+        assigneeID: d.integer().references(() => users.id),
+        createdAt: d.timestamp({ withTimezone: true })
+            .defaultNow()
+            .notNull(),
+        updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+        deletedAt: d.timestamp({ withTimezone: true }),
+    })
+)
+
 export const organizationRelation = relations(organization, ({ many }) => ({
     projects: many(projects),
+    users: many(users),
 }))
 
 export const projectRelation = relations(projects, ({ one }) => ({
@@ -58,3 +73,22 @@ export const projectRelation = relations(projects, ({ one }) => ({
         references: [organization.id],
     }),
 }))
+
+export const ticketRelation = relations(tickets, ({ one }) => ({
+    project: one(projects, {
+        fields: [tickets.projectID],
+        references: [projects.id],
+    }),
+    assignee: one(users, {
+        fields: [tickets.assigneeID],
+        references: [users.id],
+    }),
+}))
+
+export const userRelation = relations(users, ({ one }) => ({
+    organization: one(organization, {
+        fields: [users.orgID],
+        references: [organization.id],
+    }),
+}))
+
